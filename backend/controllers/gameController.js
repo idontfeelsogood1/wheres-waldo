e4b08e8e0246bbf9576e145412c1758326f04be7
Le Duong <leduongnewmemes123@gmail.com>
2025-11-03 17:45:13 +0700

    feat(backend): Implement basic controller structure

---
+++ backend/controllers/gameController.js
@@ -0,0 +1,14 @@
+const db = require("../prisma/queries")
+
+async function getGame(req, res) {
+
+}
+
+async function postMouseClick(req, res) {
+
+}
+
+module.exports = {
+    getGame,
+    postMouseClick,
+}
---
+++ backend/controllers/leaderboardController.js
@@ -0,0 +1,14 @@
+const db = require("../prisma/queries")
+
+async function getLeaderboard(req, res) {
+
+}
+
+async function postLeaderboard(req, res) {
+
+}
+
+module.exports = {
+    getLeaderboard,
+    postLeaderboard,
+}
