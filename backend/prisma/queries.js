668f5aca7f3edd090ad5cc28df5cfd8a8fa16636
Le Duong <leduongnewmemes123@gmail.com>
2025-11-04 15:51:00 +0700

    feat(backend): Implement createLeaderboardRecord

--- backend/prisma/queries.js
+++ backend/prisma/queries.js
@@ -10,6 +10,20 @@ async function findLeaderboard() {
     }
 }
 
+async function createLeaderboardRecord(username, gamename, seconds) {
+    try {
+        await prisma.leaderboard.create({
+            username: username,
+            gamename: gamename,
+            seconds: seconds,
+        })
+    } catch(err) {
+        console.log("DB error at createLeaderboardRecord: ", err)
+        throw err
+    }
+}
+
 module.exports = {
     findLeaderboard,
+    createLeaderboardRecord,
 }
