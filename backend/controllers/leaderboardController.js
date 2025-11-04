41c5271859ec8d99dc669eb246b9e301e5f7f6dc
Le Duong <leduongnewmemes123@gmail.com>
2025-11-04 15:51:44 +0700

    feat(backend): Implement getLeaderboard/postLeaderboard

--- backend/controllers/leaderboardController.js
+++ backend/controllers/leaderboardController.js
@@ -1,11 +1,24 @@
 const db = require("../prisma/queries")
 
 async function getLeaderboard(req, res) {
-
+    try {
+        const leaderboard = await db.findLeaderboard()
+        res.status(200).json({ leaderboard })
+    } catch(err) {
+        res.status(500).json({ message: "Something went wrong." })
+    }
 }
 
 async function postLeaderboard(req, res) {
-
+    try {
+        const username = req.params.username
+        const gamename = req.params.gamename
+        const seconds = req.params.seconds
+        await db.createLeaderboardRecord(username, gamename, seconds)
+        res.sendStatus(200) 
+    } catch(err) {
+        res.status(500).json({ message: "Something went wrong." })
+    }
 }
 
 module.exports = {
