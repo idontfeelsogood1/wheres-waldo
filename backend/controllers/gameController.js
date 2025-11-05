3a296f32a68b10531973be3dbe29fa7736453891
Le Duong <leduongnewmemes123@gmail.com>
2025-11-05 13:28:30 +0700

    feat(backend): Implement getGame

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,14 +1,44 @@
 const db = require("../prisma/queries")
 
 async function getGame(req, res) {
+    try {
+        req.session.score = 0
+        req.session.startTime = Date.now()
 
+        const gameId = parseInt(req.params.gameId)
+        const game = await db.findGame(gameId)
+
+        res.status(200).json({ game })
+    } catch(err) {
+        res.status(500).json({ message: "Failed to fetch game." })
+    }
 }
 
 async function postMouseClick(req, res) {
+    // VALIDATE USER'S CLICK
+    // If correct:
+        // Increment session score 
+
+    // CHECK IF USER HAS WON ( score === 3 )
+    // If won:
+        // Calculate seconds by (currentTime - startTime)
+        // Send won signal
+        // SET score/startTime TO NULL
+    // Else:
+        // Send not won signal
+}
+
+async function getGameImage(req, res) {
+
+}
+
+async function getCharacterImage(req, res) {
 
 }
 
 module.exports = {
     getGame,
     postMouseClick,
+    getGameImage,
+    getCharacterImage,
 }
