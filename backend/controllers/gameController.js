7536470428a587ae70cdd69b46c54a4da4ac96cc
Le Duong <leduongnewmemes123@gmail.com>
2025-11-10 18:41:08 +0700

    feat(backend): Implement isCorrectClick, working on postMouseClick

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -14,21 +14,51 @@ async function getGame(req, res) {
     }
 }
 
-async function postMouseClick(req, res) {
-    // VALIDATE USER'S CLICK
-    // If correct:
-        // Increment session score 
+function isCorrectClick(clickX, clickY, rectX, rectY, rectWidth, rectHeight) {
+    const isInsideX = (clickX >= rectX) && (clickX <= rectX + rectWidth)
+    const isInsideY = (clickY >= rectY) && (clickY <= rectY + rectHeight)
+    if (isInsideX && isInsideY) return true
+    return false
+}
 
-    // CHECK IF USER HAS WON ( score === 3 )
-    // If won:
-        // Calculate seconds by (currentTime - startTime)
-        // Send won signal
-        // SET score/startTime TO NULL
-    // Else:
-        // Send not won signal
+// FIGURE OUT HOW TO KNOW IF A CHARACTER HAS BEEN FOUND OR NOT
+// IF FOUND
+    // DONT ALLOW USER TO CLICK GET CORRECT SIGNAL WHEN CLICKING AT THE SAME PLACE
+async function postMouseClick(req, res) {
+    try {
+        const gameId = parseInt(req.params.gameId)
+        const characterId = parseInt(req.params.characterId)
+        const clickX = parseInt(req.body.clickX)
+        const clickY = parseInt(req.body.clickY)
+        const { x, y, width, height } = await db.findCharacterOfGame(characterId, gameId)
+
+        if (isCorrectClick(clickX, clickY, x, y, width, height)) {
+            req.session.score += 1
+            if (req.session.score === 3) {
+                res.json({
+                    isCorrectClick: true,
+                    hasWon: true,
+                    secondsFinished: Math.floor((Date.now() - req.session.startTime) / 1000)
+                })
+            } else {
+                res.json({
+                    isCorrectClick: true,
+                    hasWon: false,
+                })
+            }
+        } else {
+            res.json({ 
+                isCorrectClick: false, 
+                hasWon: false,
+            })
+        }
+    } catch(err) {
+        res.status(500).json({ message: "Failed to validate click." })
+    }
 }
 
 module.exports = {
     getGame,
     postMouseClick,
+    isCorrectClick
 }
