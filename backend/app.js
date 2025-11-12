fd54910d7c7808809fe403b00350b047ee5159b9
Le Duong <leduongnewmemes123@gmail.com>
2025-11-12 12:53:20 +0700

    feat(backend): Enable cors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 // CONFIGS
 require('dotenv').config()
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
