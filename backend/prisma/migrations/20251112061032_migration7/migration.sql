fb3afcdd9d03450c4b1a2e7954a7368960b7d69e
Le Duong <leduongnewmemes123@gmail.com>
2025-11-12 13:12:15 +0700

    change(backend): Add migration

---
+++ backend/prisma/migrations/20251112061032_migration7/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `name` to the `Character` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `Game` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Character" ADD COLUMN     "name" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Game" ADD COLUMN     "name" TEXT NOT NULL;
